912dd14cf62aed7e17be42ed9c712f3e1e5fa959
Abel Molina <amolina@c3r12s1.42madrid.com>
2025-10-17 17:41:17 +0200

    1st push ft_pfrintf

---
+++ libftprintf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amolina <amolina@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 13:54:49 by amolina           #+#    #+#             */
+/*   Updated: 2025/10/17 14:11:07 by amolina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+// LIBRERIES
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+
+// FUNCTIONS
+
+int	ft_printf(char const *, ...);
+
+#endif
